24ff5cd5fbf7295ca495118c2a59b5423614c9b9
jtech-dev <jtech-dev>
2018-01-24 11:29:05 +0900

    Add doctorin sample files

---
+++ src/Entity/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2, nullable=true)
+     */
+    private $price;
+
+    // add your own fields
+}
